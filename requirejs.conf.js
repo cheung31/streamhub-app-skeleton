78a1a17eb086efabbbccb5b6ddd53fe1746e1d67
Ryan Cheung <ryanc@livefyre.com>
2013-10-28 13:38:19 -0700

    Include 'json' module in requirejs.conf

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -8,6 +8,7 @@ requirejs.config({
     text: 'lib/requirejs-text/text',
     hgn: 'lib/requirejs-hogan-plugin/hgn',
     hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
+    json: 'lib/requirejs-plugins/src/json',
     base64: 'lib/base64/base64',
     'event-emitter': 'lib/event-emitter/src/event-emitter',
     inherits: 'lib/inherits/inherits'
