6f9286cd8300b0e2e167e90e7c7271d5f517b7c0
Ryan Cheung <ryanc@livefyre.com>
2013-11-08 11:13:34 -0800

    Add example.js skeleton for App Gallery live demo

---
+++ example.js
@@ -0,0 +1,17 @@
+// This is what the App Gallery uses for your app's live demo on its App Page
+
+define(function(require) {
+    // 1) Require all the modules you need. Tip: Your examples/ dir might already
+    //    have an example that would be a great starting point.
+
+    return function(el) {
+        // 2) Instantiate your app and pipe in a collection here!
+        //
+        // el is the containing element for your example. (e.g. the live demo on
+        // the App Page)
+
+        var view;
+  
+        return view;
+    };
+});
